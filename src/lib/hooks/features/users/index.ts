b17a7ab162ed054c93670787809475851404081a
Pinpie-27 <hnnguyen0327@gmail.com>
2025-03-28 00:46:55 +0700

    fix: error on vercel 2

--- src/lib/hooks/features/users/index.ts
+++ src/lib/hooks/features/users/index.ts
@@ -1,4 +1,4 @@
-export * from './get-user'
-export * from './delete-user'
-export { updateUserById, formStructure } from './update-user';
-export * from './search-user'
+export * from './get-user';
+export * from './delete-user';
+export { updateUserById } from './update-user';
+export { formStructureSearch } from './search-user';
